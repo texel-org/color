8c18f85a625c6a1c337663b02ede089c1bde3dd3
mattdesl <info@mattdesl.studio>
2025-03-20 10:19:44 +0000

    update sUCS examples

--- test/canvas-sucs-gamut.js
+++ test/canvas-sucs-gamut.js
@@ -1,32 +1,22 @@
 const canvasSketch = require("canvas-sketch");
 import {
   convert,
-  degToRad,
   DisplayP3Gamut,
   gamutMapOKLCH,
-  hexToRGB,
   isRGBInGamut,
-  lerp,
-  lerpAngle,
-  MapToAdaptiveCuspL,
-  MapToAdaptiveGray,
-  MapToCuspL,
   MapToL,
-  OKHSL,
-  OKLab,
   OKLCH,
-  radToDeg,
   serialize,
-  sRGB,
   sRGBGamut,
-  sRGBLinear,
 } from "../src/index.js";
-import { sLab, sLCH } from "./spaces/simple-ucs.js";
-import { Lab } from "./spaces/lab.js";
-import { OKLrab } from "./spaces/oklrab.js";
+import { sLCH } from "./spaces/simple-ucs.js";
 
 const settings = {
-  dimensions: [500, 500],
+  duration: 10,
+  fps: 12,
+  playbackRate: "throttle",
+  animate: true,
+  dimensions: [256, 256],
 };
 
 const sketch = ({ context }) => {
@@ -35,22 +25,37 @@ const sketch = ({ context }) => {
   const mapping = MapToL;
   const outputSpace = gamut.space;
 
-  return ({ context, width, height }) => {
+  return ({ context, width, height, playhead }) => {
     context.fillStyle = "gray";
     context.fillRect(0, 0, width, height);
+
+    const theta = playhead * 360;
+
+    // sLCH exhibits an unusual behavior in some blue hue planes
+    // for example here, there is a 'slice' or 'gap' where it goes out of sRGB gamut
+    // const theta = 251.42954999947546;
 
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         const u = x / width;
         const v = y / height;
 
-        const L = 1 - v;
-        const C = u * 0.4;
-        const H = 90;
-
-        const oklch = [L, C, H];
-        const outRGB = convert(oklch, OKLCH, outputSpace);
+        const inputSpace = sLCH;
+        let lch;
+        if (inputSpace.id === "slch") {
+          const L = (1 - v) * 100;
+          const C = u * 55;
+          const H = theta;
+          lch = [L, C, H];
+        } else if (inputSpace.id === "oklch") {
+          const L = 1 - v;
+          const C = 0.35 * u;
+          const H = theta;
+          lch = [L, C, H];
+        }
+        const outRGB = convert(lch, inputSpace, outputSpace);
         if (isRGBInGamut(outRGB)) {
+          const oklch = convert(lch, inputSpace, OKLCH);
           const mappedRGB = gamutMapOKLCH(
             oklch,
             gamut,
--- test/spaces/simple-ucs.js
+++ test/spaces/simple-ucs.js
@@ -32,6 +32,14 @@ export const sLab_to_sLMSp_M = [
   [0.009999999999999998, -0.00036429872495446266, -0.010185494963012527],
 ];
 
+const POWER = 0.43;
+const INV_POWER = 1.0 / POWER;
+
+const forwardPower = (v) =>
+  v >= 0 ? Math.pow(v, POWER) : -Math.pow(-v, POWER);
+const invPower = (v) =>
+  v >= 0 ? Math.pow(v, INV_POWER) : -Math.pow(-v, INV_POWER);
+
 /**
  * @type {ColorSpace}
  * @category spaces
@@ -39,20 +47,28 @@ export const sLab_to_sLMSp_M = [
 export const sLab = {
   id: "slab",
   toXYZ(Lab, out = vec3()) {
-    const invPower = 1.0 / 0.43;
-    const sLMSp = transform(Lab, sLab_to_sLMSp_M);
-    const sLMS = sLMSp.map((v) => {
-      return v >= 0 ? Math.pow(v, invPower) : -Math.pow(-v, invPower);
-    });
-    return transform(sLMS, sLMS_to_XYZ_D65_M, out);
+    // sLab --> sLMSp
+    const sLMSp = transform(Lab, sLab_to_sLMSp_M, out);
+
+    // sLMSp --> sLMS
+    sLMSp[0] = invPower(sLMSp[0]);
+    sLMSp[1] = invPower(sLMSp[1]);
+    sLMSp[2] = invPower(sLMSp[2]);
+
+    // sLMS --> XYZ D65
+    return transform(sLMSp, sLMS_to_XYZ_D65_M, out);
   },
   fromXYZ(xyz, out = vec3()) {
-    const power = 0.43;
-    const sLMS = transform(xyz, XYZ_D65_to_sLMS_M);
-    const sLMSp = sLMS.map((v) => {
-      return v >= 0 ? Math.pow(v, power) : -Math.pow(-v, power);
-    });
-    return transform(sLMSp, sLMSp_to_sLab_M, out);
+    // XYZ D65 --> sLMS
+    const sLMS = transform(xyz, XYZ_D65_to_sLMS_M, out);
+
+    // sLMS --> sLMSp
+    sLMS[0] = forwardPower(sLMS[0]);
+    sLMS[1] = forwardPower(sLMS[1]);
+    sLMS[2] = forwardPower(sLMS[2]);
+
+    // sLMSp --> sLab
+    return transform(sLMS, sLMSp_to_sLab_M, out);
   },
 };
 
@@ -98,43 +114,45 @@ export const sLCH = {
 // const Fl =
 // const Z = 1.48 + Math.sqrt((Yb/Yw))
 
-const xyzD65 = convert([0.5, 0.25, 0.75], sRGB, XYZ);
-const lab = convert(xyzD65, XYZ, sLab);
-const xyz = convert(lab, sLab, XYZ);
-console.log("Input XYZ D65", xyzD65);
-console.log("Lab", lab);
-console.log("XYZ From Lab", xyz);
-const lch = convert(lab, sLab, sLCH);
-const lab1 = convert(lch, sLCH, sLab);
-console.log("LCH", lch);
-console.log("Lab from LCH", lab1);
+// function check() {
+//   const xyzD65 = convert([0.5, 0.25, 0.75], sRGB, XYZ);
+//   const lab = convert(xyzD65, XYZ, sLab);
+//   const xyz = convert(lab, sLab, XYZ);
+//   console.log("Input XYZ D65", xyzD65);
+//   console.log("Lab", lab);
+//   console.log("XYZ From Lab", xyz);
+//   const lch = convert(lab, sLab, sLCH);
+//   const lab1 = convert(lch, sLCH, sLab);
+//   console.log("LCH", lch);
+//   console.log("Lab from LCH", lab1);
 
-const labBounds = [
-  [Infinity, Infinity, Infinity],
-  [-Infinity, -Infinity, -Infinity],
-];
-const lchBounds = [
-  [Infinity, Infinity, Infinity],
-  [-Infinity, -Infinity, -Infinity],
-];
+//   const labBounds = [
+//     [Infinity, Infinity, Infinity],
+//     [-Infinity, -Infinity, -Infinity],
+//   ];
+//   const lchBounds = [
+//     [Infinity, Infinity, Infinity],
+//     [-Infinity, -Infinity, -Infinity],
+//   ];
 
-const steps = 4;
-for (let r = 0; r < 256; r += steps) {
-  for (let g = 0; g < 256; g += steps) {
-    for (let b = 0; b < 256; b += steps) {
-      const rgb = [r, g, b].map((v) => v / 255);
-      const xyz = convert(rgb, sRGB, XYZ);
-      const lab = convert(xyz, XYZ, sLab);
-      const lch = convert(lab, sLab, sLCH);
-      for (let i = 0; i < 3; i++) {
-        labBounds[0][i] = Math.min(labBounds[0][i], lab[i]);
-        labBounds[1][i] = Math.max(labBounds[1][i], lab[i]);
-        lchBounds[0][i] = Math.min(lchBounds[0][i], lch[i]);
-        lchBounds[1][i] = Math.max(lchBounds[1][i], lch[i]);
-      }
-    }
-  }
-}
+//   const steps = 4;
+//   for (let r = 0; r < 256; r += steps) {
+//     for (let g = 0; g < 256; g += steps) {
+//       for (let b = 0; b < 256; b += steps) {
+//         const rgb = [r, g, b].map((v) => v / 255);
+//         const xyz = convert(rgb, sRGB, XYZ);
+//         const lab = convert(xyz, XYZ, sLab);
+//         const lch = convert(lab, sLab, sLCH);
+//         for (let i = 0; i < 3; i++) {
+//           labBounds[0][i] = Math.min(labBounds[0][i], lab[i]);
+//           labBounds[1][i] = Math.max(labBounds[1][i], lab[i]);
+//           lchBounds[0][i] = Math.min(lchBounds[0][i], lch[i]);
+//           lchBounds[1][i] = Math.max(lchBounds[1][i], lch[i]);
+//         }
+//       }
+//     }
+//   }
 
-console.log("Lab Bounds", labBounds);
-console.log("LCH Bounds", lchBounds);
+//   console.log("Lab Bounds", labBounds);
+//   console.log("LCH Bounds", lchBounds);
+// }
